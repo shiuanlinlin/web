0fda4df430f51bd8113d672741a844120d8ae8b3
shiuanlinlin <9611129@gmail.com>
2020-08-19 11:10:17 +0800

    first commit

---
+++ js/array.js
@@ -0,0 +1,18 @@
+var wkarray =[
+			//['名稱','網頁畫版、切版','網址'],
+			['麗嬰房2018春夏網站','網頁畫版、切版','https://www.phland.com.tw/event/2018ss/','https://farm1.staticflickr.com/802/40673945794_7100741ea4_b.jpg'],
+			['怡聚科技','網頁畫版、切版','http://www.ichutechnology.com/tw/','https://farm1.staticflickr.com/871/39578870450_dc29d894f8_b.jpg'],
+			// ['登山用品','1',''],
+			
+			['南投縣農會','網頁畫版、切版','http://www.nthfa.org.tw/','https://farm1.staticflickr.com/816/40673945954_7a1162045a_z.jpg'],
+			['大鈞醫學美容','網頁切版','http://www.bigbeauty.com.tw/','https://farm1.staticflickr.com/815/39578870670_de200422ae_z.jpg'],
+			//['環渼節能科技股份有限公司','網頁切版','http://www.escomaster.com.tw/','https://farm1.staticflickr.com/794/27518860248_c479a0f9f9_z.jpg'],
+			// ['防潮家','1','http://www.satv.com.tw/index.php'],
+			// ['U\'re','1',''],
+			['約客頂級汽車旅館','網頁畫版、切版','http://www.yorker.com.tw/','https://farm1.staticflickr.com/795/39578870770_04e5c79b7e_b.jpg'],
+			//['花蓮縣醫師公會','網頁畫版、切版','http://www.hma.org.tw/','https://farm1.staticflickr.com/874/41388799331_b75b6b1d5e_z.jpg'],
+			['麗嬰房2018世界地球日','網頁畫版、切版','https://www.phland.com.tw/event/2018earthday/index.php','https://farm1.staticflickr.com/808/39578870890_c3ce5e2b0a_z.jpg'],
+			['Ure日系服裝','網頁畫版、切版','https://ure-ure.com.tw/','https://farm1.staticflickr.com/914/41254587020_85b95337bc_z.jpg'],
+			['維克爾科技儀器有限公司','網頁畫版、切版','http://weikeer.com/','https://farm1.staticflickr.com/808/40673946284_cb94b23b44_z.jpg'],
+			['伯東國際商通','網頁畫版、切版','http://www.hakuto.com.tw/tw/index.php','https://farm1.staticflickr.com/805/27518858728_1fcbf1bccb_z.jpg'],
+];
---
+++ js/array2.js
@@ -0,0 +1,19 @@
+var wkarray =[
+			//['名稱','網頁畫版、切版','網址'],
+			
+			['怡聚科技','網頁畫版、切版','https://drive.google.com/drive/folders/1DOBPAH3ZtZX-1DANcHy92wLbl01-XHkI','https://farm1.staticflickr.com/871/39578870450_dc29d894f8_b.jpg'],
+			['山泉酒糟雞','網頁畫版、切版','https://drive.google.com/drive/folders/12taixliQER13LEDhaQqAGbFy7ex0R9ge','https://farm1.staticflickr.com/808/40493369335_a45f4a1c6f_z.jpg'],
+			['偉立達','網頁畫版、切版','https://drive.google.com/drive/folders/1Hy7a1F5tTVPG3yVXsHo29atredR-AC4Y','https://farm2.staticflickr.com/1824/43015788712_3a26cb1655_z.jpg'],
+			['瀛海科技','網頁畫版、切版','https://drive.google.com/drive/folders/1EBBF_Ul6qtOaLKwImebx6c7953Cc7P16','https://farm2.staticflickr.com/1823/42162185785_c5b46751d3_z.jpg'],
+			// ['登山用品','1',''],
+			//['伯東國際商通','網頁畫版、切版','http://www.hakuto.com.tw/tw/index.php','https://farm1.staticflickr.com/805/27518858728_1fcbf1bccb_z.jpg'],
+			['南投縣農會','網頁畫版、切版','https://drive.google.com/drive/folders/1IUm_Fa5k7sKzDt0xmbSj_K3gZx1u-tvX','https://farm1.staticflickr.com/816/40673945954_7a1162045a_z.jpg'],
+			//['大鈞醫學美容','網頁切版','http://www.bigbeauty.com.tw/','https://farm1.staticflickr.com/815/39578870670_de200422ae_z.jpg'],
+			//['環渼節能科技股份有限公司','網頁切版','http://www.escomaster.com.tw/','https://farm1.staticflickr.com/794/27518860248_c479a0f9f9_z.jpg'],
+			['Ure日系服裝','網頁畫版、切版','https://drive.google.com/drive/folders/11Vt-iCkNegAMnjAXZaPDbuvSv3nfwGQE','https://farm1.staticflickr.com/914/41254587020_85b95337bc_z.jpg'],
+			['防潮家','網頁畫版、切版','https://drive.google.com/drive/folders/1UAeqEVj9vV-a5TrCa4dyOfZ8KIf3U8Th','https://farm2.staticflickr.com/1802/43065468541_54372eba97_z.jpg'],
+			// ['U\'re','1',''],			
+			//['花蓮縣醫師公會','網頁畫版、切版','http://www.hma.org.tw/','https://farm1.staticflickr.com/874/41388799331_b75b6b1d5e_z.jpg'],
+			//['麗嬰房2018世界地球日','網頁畫版、切版','https://www.phland.com.tw/event/2018earthday/index.php','https://farm1.staticflickr.com/808/39578870890_c3ce5e2b0a_z.jpg'],			
+			['麗嬰房-心理測驗','網頁畫版、切版','https://drive.google.com/drive/folders/1w540sjH5RXmcNu0NMyYzZqNUiTVcfrpP','https://farm1.staticflickr.com/802/40673945794_7100741ea4_b.jpg'],
+];
